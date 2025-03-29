d15d084d4854b65ca777c3180d47df8498f8f431
elmage78 <elmagesmm@gmail.com>
2025-03-29 11:47:09 +0100

    hi

--- src/main/java/gal/uvigo/esei/aed1/chupatedos/core/Game.java
+++ src/main/java/gal/uvigo/esei/aed1/chupatedos/core/Game.java
@@ -14,6 +14,7 @@ public class Game {
   * Metodo principal para jugar
   */
   public void play() {
+    System.out.println("hi");
 
     
 
--- src/main/java/gal/uvigo/esei/aed1/chupatedos/core/Player.java
+++ src/main/java/gal/uvigo/esei/aed1/chupatedos/core/Player.java
@@ -3,6 +3,8 @@ package gal.uvigo.esei.aed1.chupatedos.core;
 
 
 public class Player {
-
+    public Player(DeckOfCards a){
+        
+    }
     
 }
