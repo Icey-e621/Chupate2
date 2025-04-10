c1738ffb8aa5f249a22be25639d775f9194fe6b1
Hectodroid <ourensehector@gmail.com>
2025-04-10 17:39:24 +0200

    borrado

--- src/main/java/gal/uvigo/esei/aed1/chupatedos/core/Table.java
+++ src/main/java/gal/uvigo/esei/aed1/chupatedos/core/Table.java
@@ -2,7 +2,7 @@ package gal.uvigo.esei.aed1.chupatedos.core;
 
 import java.util.Stack;
 
-public class Table {}
+public class Table {
     private Stack<Card> descartes;
     /**
      *  Constructor for the Table class
@@ -21,31 +21,5 @@ public class Table {}
             throw new IllegalStateException("No hay carta en la mesa");
         }
         return descartes.peek();
-    }
-     
-    /** 
-     * Metodo para coger una carta de la baraja
-     * si la baraja esta vacia la llena y la mezcla
-     * @return la carta cogida de la baraja
-     */
-    public Card takeCard() {
-        if (this.deck.isEmpty()) {
-            while(!descartes.isEmpty()) {
-                this.deck.addCard(this.descartes.pop());
-            }
-        }
-        return deck.pop();
-    }
-    /**
-     * Metodo para tirar una carta a la mesa
-     * si la carta es nula lanza una excepcion
-     * @param card
-     */
-    public void throwCard(Card card) {
-        if (card == null) {
-            throw new IllegalArgumentException("Card no puede ser nula");
-        }
-        this.descartes.push(card);
-        this.topCard = card;
     }
 }
